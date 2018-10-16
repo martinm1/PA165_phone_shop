e753c5d641200f17c712271fe693c8d0fd6b5199
Stefan Holecko <stevoh096@gmail.com>
2018-10-16 15:34:07 +0200

    Stock entity done

---
+++ project/src/main/java/cz/muni/fi/pa165/smartphonEShop/dao/StockDao.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.pa165.smartphonEShop.dao;
+
+/**
+ * Created by Stefan Holecko
+ * Interface represents:
+ */
+public interface StockDao {
+}
--- project/src/main/java/cz/muni/fi/pa165/smartphonEShop/entity/Stock.java
+++ project/src/main/java/cz/muni/fi/pa165/smartphonEShop/entity/Stock.java
@@ -1,9 +1,70 @@
 package cz.muni.fi.pa165.smartphonEShop.entity;
 
+import javax.persistence.*;
+
 /**
  * Created by Stefan Holecko
  * Class represents:
  */
 
+@Entity
 public class Stock {
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable=false,unique=true)
+    private String name;
+
+    @Column(nullable=false)
+    private Long addressId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Long addressId) {
+        this.addressId = addressId;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (! (obj instanceof Stock))
+            return false;
+        Stock other = (Stock) obj;
+        if (name == null) {
+            if (other.getName() != null)
+                return false;
+        } else if (!name.equals(other.getName()))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
 }
