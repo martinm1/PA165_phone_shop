f6635864d1149a4df2e4322ea37396bc01c3ada9
Stefan Holecko <stevoh096@gmail.com>
2018-10-16 15:51:41 +0200

    stock Entity changed setId removed

--- project/src/main/java/cz/muni/fi/pa165/smartphonEShop/entity/Stock.java
+++ project/src/main/java/cz/muni/fi/pa165/smartphonEShop/entity/Stock.java
@@ -23,9 +23,9 @@ public class Stock {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
 
     public String getName() {
         return name;
