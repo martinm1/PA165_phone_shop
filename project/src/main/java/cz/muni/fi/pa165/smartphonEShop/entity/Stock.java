fab81b465a3e7e5bcc7c84a13178a1f1533b8be3
Stefan Holecko <stevoh096@gmail.com>
2018-10-29 16:23:47 +0100

    diagrams edited

--- project/src/main/java/cz/muni/fi/pa165/smartphonEShop/entity/Stock.java
+++ project/src/main/java/cz/muni/fi/pa165/smartphonEShop/entity/Stock.java
@@ -28,8 +28,8 @@ public class Stock {
     @Column(nullable=false,unique=true)
     private String name;
 
-//    @Column(nullable=false)
-//    private Long addressId;
+    @Column(nullable=false)
+    private Long addressId;
 
     @OneToMany(mappedBy = "stock") //TODO mappedBy?
     private Set<Phone> phones = new HashSet<>();
