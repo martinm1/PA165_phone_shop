3f80e66291b2f136b9c4b6c7a871a7bf929e9419
naheCZ <nahalkaroman@gmail.com>
2018-11-19 18:17:30 +0100

    AddressServiceTest file prepared

--- service/src/test/java/cz/muni/fi/pa165/smartphonEShop/service/service/AddressServiceTest.java
+++ service/src/test/java/cz/muni/fi/pa165/smartphonEShop/service/service/AddressServiceTest.java
@@ -1,11 +1,17 @@
 package cz.muni.fi.pa165.smartphonEShop.service.service;
 
 
+import cz.muni.fi.pa165.smartphonEShop.service.config.ServiceConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
 /**
  * Created by Roman Nahalka
  * Class represents: Tests for AddressService.
  */
-public class AddressServiceTest
+
+@ContextConfiguration(classes = ServiceConfiguration.class)
+public class AddressServiceTest extends AbstractTestNGSpringContextTests
 {
 
 }
