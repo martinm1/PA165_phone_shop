f689b15a61b9b5181bbfa7efdb4808f97b0182a9
Stefan Holecko <stevoh096@gmail.com>
2018-11-19 15:49:24 +0100

    ClaimFacade and StockFacade implemented with JavaDoc

--- api/src/main/java/cz/muni/fi/pa165/smartphonEShop/facade/ClaimFacade.java
+++ api/src/main/java/cz/muni/fi/pa165/smartphonEShop/facade/ClaimFacade.java
@@ -1,6 +1,8 @@
 package cz.muni.fi.pa165.smartphonEShop.facade;
 
 import cz.muni.fi.pa165.smartphonEShop.dto.ClaimDTO;
+import cz.muni.fi.pa165.smartphonEShop.enums.ClaimSolution;
+import cz.muni.fi.pa165.smartphonEShop.enums.ClaimState;
 
 import java.util.Collection;
 import java.util.List;
@@ -12,5 +14,45 @@ import java.util.List;
 
 public interface ClaimFacade {
 
+    /**
+     * Find claim with specific id
+     * @param id primary key for claim
+     * @return claim with given id.
+     */
+    ClaimDTO findClaimById(Long id);
+
+    /**
+     * Find claims with specific orderId
+     * @param orderId of the  claim
+     * @return Collection of claims with given orderId.
+     */
+    Collection<ClaimDTO> findClaimByOrderId(Long orderId);
+
+    /**
+     * Find claims with specific userId
+     * @param userId of the  claim
+     * @return Collection of claims with given userId.
+     */
+    Collection<ClaimDTO> findClaimByUserId(Long userId);
+
+    /**
+     * Find claims with specific claimState
+     * @param claimState of the  claim
+     * @return Collection of claims with given claimState.
+     */
+    Collection<ClaimDTO> findClaimByClaimState(ClaimState claimState);
+
+    /**
+     * Find claims with specific claimSolution
+     * @param claimSolution of the  claim
+     * @return Collection of claims with given claimSolution.
+     */
+    Collection<ClaimDTO> findClaimByClaimSolution(ClaimSolution claimSolution);
+
+    /**
+     * Find all claims.
+     * @return Collection of all claims.
+     */
+    Collection<ClaimDTO> getAllClaims();
 
 }
--- api/src/main/java/cz/muni/fi/pa165/smartphonEShop/facade/StockFacade.java
+++ api/src/main/java/cz/muni/fi/pa165/smartphonEShop/facade/StockFacade.java
@@ -1,9 +1,44 @@
 package cz.muni.fi.pa165.smartphonEShop.facade;
 
+import cz.muni.fi.pa165.smartphonEShop.dto.StockDTO;
+import cz.muni.fi.pa165.smartphonEShop.entity.Address;
+
+import java.util.Collection;
+
 /**
  * Created by Stefan Holecko
  * Class represents:
  */
 
 public interface StockFacade {
+
+    /**
+     * Find stock with specific id
+     * @param id primary key for stock
+     * @return Stock with given id.
+     */
+    StockDTO findStockById(Long id);
+
+    /**
+     * Find stock with specific name
+     * @param name of the  stock
+     * @return Stock with given name.
+     */
+    StockDTO findStockByName(String name);
+
+    /**
+     * Find stock with specific addressId
+     * @param addressId of the  stock
+     * @return Stock with given address.
+     */
+    StockDTO findStockByAddressId(Long addressId);
+    
+
+    /**
+     * Find all stocks.
+     * @return Collection of all stocks.
+     */
+    Collection<StockDTO> getAllStocks();
+
+
 }
