c424eec4da6c190f39dbf39698f3942b345bcafa
Martin Mejzlík <xmejzli2@fi.muni.cz>
2018-11-21 21:34:42 +0100

    hope this can fix this

---
+++ service/src/main/java/cz/muni/fi/pa165/smartphonEShop/service/service/OrderServiceImpl.java
@@ -0,0 +1,66 @@
+package cz.muni.fi.pa165.smartphonEShop.service.service;
+
+import cz.muni.fi.pa165.smartphonEShop.dao.OrderDao;
+import cz.muni.fi.pa165.smartphonEShop.entity.Order;
+import cz.muni.fi.pa165.smartphonEShop.enums.OrderState;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+//
+/**
+ *
+ * @author martin
+ */
+public class OrderServiceImpl implements OrderService{
+
+    @Autowired
+    private OrderDao orderDao;
+    
+    @Override
+    public Order findOrderById(Long id) {
+        return orderDao.findById(id);
+    }
+
+    @Override
+    public List<Order> findOrdersByOrderState(OrderState state) {
+        return orderDao.findOrdersByOrderState(state);
+    }
+
+    @Override
+    public List<Order> findOrdersByOrderDate(LocalDate orderDate) {
+        return orderDao.findOrdersByOrderDate(orderDate);
+    }
+
+    @Override
+    public List<Order> findOrdersByPerson(String personId) {
+        return orderDao.findOrdersByPerson(personId);
+    }
+
+    @Override
+    public List<Order> findOrdersByPhone(Long phoneId) {
+        return orderDao.findOrdersByPhone(phoneId);
+    }
+   
+
+    @Override
+    public List<Order> getAllOrders() {
+        return orderDao.findAll();
+    }
+
+    @Override
+    public void addClaim(Long orderId, Long claimId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Long createOrder(Order order) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeClaim(Long orderId, Long claimId) {
+
+    }
+}
