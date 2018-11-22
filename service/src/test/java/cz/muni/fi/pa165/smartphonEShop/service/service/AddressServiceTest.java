dd7aa3b3b8d3e33995dc39e631009a3c4d6d1720
naheCZ <nahalkaroman@gmail.com>
2018-11-22 13:36:05 +0100

    Address service tests

--- service/src/test/java/cz/muni/fi/pa165/smartphonEShop/service/service/AddressServiceTest.java
+++ service/src/test/java/cz/muni/fi/pa165/smartphonEShop/service/service/AddressServiceTest.java
@@ -3,11 +3,14 @@ package cz.muni.fi.pa165.smartphonEShop.service.service;
 
 import cz.muni.fi.pa165.smartphonEShop.dao.AddressDao;
 import cz.muni.fi.pa165.smartphonEShop.entity.Address;
+import cz.muni.fi.pa165.smartphonEShop.enums.AddressEnum;
 import cz.muni.fi.pa165.smartphonEShop.service.config.ServiceConfiguration;
 import org.hibernate.service.spi.ServiceException;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
@@ -16,7 +19,14 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Roman Nahalka
@@ -58,10 +68,68 @@ public class AddressServiceTest extends AbstractTestNGSpringContextTests
     @Test
     public void getAllTest()
     {
+        when(addressDao.findAll()).thenReturn(Stream.of(address1, address2).collect(Collectors.toList()));
+
         List<Address> addresses = addressService.getAllAddresses();
 
         Assert.assertEquals(2, addresses.size());
         Assert.assertTrue(addresses.contains(address1));
         Assert.assertTrue(addresses.contains(address2));
+    }
+
+    @Test
+    public void findAddressById()
+    {
+        address1.setId(10L);
+        address2.setId(20L);
+
+        when(addressDao.findById(address1.getId())).thenReturn(address1);
+        when(addressDao.findById(address2.getId())).thenReturn(address2);
+
+        Address address = addressService.findAddressById(address1.getId());
+        Address address3 = addressService.findAddressById(address2.getId());
+
+        Assert.assertEquals(address, address1);
+        Assert.assertEquals(address3, address2);
+    }
+
+    @Test
+    public void createAddress()
+    {
+        Address address = new Address();
+
+        doAnswer(invocationOnMock ->
+        {
+            address.setId(10L);
+            return 10L;
+        }).when(addressDao).create(address);
+
+        addressService.createAddress(address);
+        Assert.assertNotNull(address.getId());
+        Assert.assertEquals(10L, address.getId().longValue());
+    }
+
+    @Test
+    public void findAllAddressesBy()
+    {
+        HashMap<AddressEnum, String> hashMap = new HashMap<>();
+        HashMap<AddressEnum, String> hashMap1 = new HashMap<>();
+
+        hashMap.put(AddressEnum.CITY, "Brno");
+        hashMap.put(AddressEnum.COUNTRY, "Czech");
+
+        hashMap1.put(AddressEnum.CITY, "Bratislava");
+        hashMap1.put(AddressEnum.COUNTRY, "Slovakia");
+
+        when(addressDao.findAllAddressesBy(hashMap)).thenReturn(Stream.of(address1).collect(Collectors.toList()));
+        when(addressDao.findAllAddressesBy(hashMap1)).thenReturn(Stream.of(address2).collect(Collectors.toList()));
+
+        List<Address> addresses =  addressService.findAllAddressesBy(hashMap);
+        List<Address> addresses1 = addressService.findAllAddressesBy(hashMap1);
+
+        Assert.assertTrue(addresses.contains(address1));
+        Assert.assertFalse(addresses.contains(address2));
+        Assert.assertTrue(addresses1.contains(address2));
+        Assert.assertFalse(addresses1.contains(address1));
     }
 }
