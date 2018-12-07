ac7136590f8706c0594aa8b9865806699aeba9fa
Martin Mejzlík <xmejzli2@fi.muni.cz>
2018-12-07 20:03:10 +0100

    StockController: added view

--- spring-mvc/src/main/java/cz/muni/fi/pa165/smartphonEShop/mvc/controllers/StockController.java
+++ spring-mvc/src/main/java/cz/muni/fi/pa165/smartphonEShop/mvc/controllers/StockController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -43,5 +44,12 @@ public class StockController {
         log.debug("new()");
         model.addAttribute("stockCreate", new StockCreateDTO());
         return "stock/new";
+    }
+    
+    @RequestMapping(value = "/view/{id}", method = RequestMethod.GET)
+    public String view(@PathVariable("id") long id, Model model)
+    {
+        model.addAttribute("stock", stockFacade.findStockById(id));
+        return "stock/view";
     }
 }
