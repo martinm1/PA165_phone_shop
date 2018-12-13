8685e1db103db72fa5dca404e0d6d34718800446
Jakub <jakubko.ondrusek@gmail.com>
2018-12-13 09:25:54 +0100

    rest tests on the way. Backboned,

--- rest/src/test/java/cz/muni/fi/pa165/smartphonEShop/rest/AddressControlerTest.java
+++ rest/src/test/java/cz/muni/fi/pa165/smartphonEShop/rest/AddressControlerTest.java
@@ -1,4 +1,60 @@
 package cz.muni.fi.pa165.smartphonEShop.rest;
 
+import cz.muni.fi.pa165.smartphonEShop.RootWebContext;
+import cz.muni.fi.pa165.smartphonEShop.dto.AddressDTO;
+import cz.muni.fi.pa165.smartphonEShop.dto.PhoneDTO;
+import cz.muni.fi.pa165.smartphonEShop.enums.Manufacturer;
+import cz.muni.fi.pa165.smartphonEShop.facade.AddressFacade;
+import cz.muni.fi.pa165.smartphonEShop.facade.PhoneFacade;
+import cz.muni.fi.pa165.smartphonEShop.rest.controllers.AddressesController;
+import cz.muni.fi.pa165.smartphonEShop.rest.controllers.PhonesController;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+/**
+ * Created by Jakub Ondrusek
+ * Class represents: Tests for address rest controller.
+ */
+@WebAppConfiguration
+@ContextConfiguration(classes = {RootWebContext.class})
 public class AddressControlerTest {
+    @Mock
+    private AddressFacade addressFacade;
+
+    @Autowired
+    @InjectMocks
+    private AddressesController addressesController;
+
+    private MockMvc mockMvc;
+
+    private AddressDTO addressDTO;
+
+    @BeforeClass
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = standaloneSetup(addressesController).setMessageConverters(new MappingJackson2HttpMessageConverter())
+                .build();
+    }
+
+    @BeforeMethod
+    public void testPrepare() {
+       addressDTO = new AddressDTO();
+
+       addressDTO.setId(111L);
+       addressDTO.setCity("Brno");
+       addressDTO.setCountry("cesko");
+       addressDTO.setStreetName("hrncirska");
+       addressDTO.setStreetNumber("bohviekolko");
+    }
+
 }
--- rest/src/test/java/cz/muni/fi/pa165/smartphonEShop/rest/StockControllerTest.java
+++ rest/src/test/java/cz/muni/fi/pa165/smartphonEShop/rest/StockControllerTest.java
@@ -3,14 +3,21 @@ package cz.muni.fi.pa165.smartphonEShop.rest;
 import cz.muni.fi.pa165.smartphonEShop.dto.StockDTO;
 import cz.muni.fi.pa165.smartphonEShop.facade.StockFacade;
 import cz.muni.fi.pa165.smartphonEShop.mvc.controllers.StockController;
+import cz.muni.fi.pa165.smartphonEShop.rest.controllers.PhonesController;
+import cz.muni.fi.pa165.smartphonEShop.rest.controllers.StocksController;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 /**
  * Created by Jakub Ondrusek
@@ -21,6 +28,10 @@ public class StockControllerTest {
     @Mock
     private StockFacade stockFacade;
 
+    @Autowired
+    @InjectMocks
+    private StocksController stocksController;
+
     private StockDTO stockDTO;
 
     private MockMvc mockMvc;
@@ -28,17 +39,17 @@ public class StockControllerTest {
     @BeforeClass
     public void setup() {
         MockitoAnnotations.initMocks(this);
-
-        StockController stockController = new StockController();
-        stockController.setStockFacade(stockFacade);
-
-        mockMvc = MockMvcBuilders.standaloneSetup(stockController).build();
+        mockMvc = standaloneSetup(stocksController).setMessageConverters(new MappingJackson2HttpMessageConverter())
+                .build();
     }
 
     @BeforeMethod
     public void testPrepare() {
         stockDTO = new StockDTO();
         stockDTO.setId(111L);
+        //stockDTO.setAddress();
+        //stockDTO.setName();
+        //stockDTO.setPhones();
     }
 
     @Test
